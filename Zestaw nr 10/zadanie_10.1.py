0b75dbab5729fa66a08d661b774d054e29ef338a
Natalia Mędrek <147911714+NatkaMed25@users.noreply.github.com>
2024-01-10 09:11:00 +0100

    Rozwiązanie zestawu nr 10

---
+++ Zestaw nr 10/zadanie_10.1.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import random 
+  
+window = Tk() 
+window.configure(bg="darkgrey") 
+window.geometry("300x200") 
+window.title("Rzut Kostką") 
+window.resizable(0, 0) 
+  
+def roll(): 
+    dice_dots = ['1', '2', '3', '4', '5', '6'] 
+    label.configure( 
+        text=f'{random.choice(dice_dots)}') 
+    label.pack() 
+   
+roll_button = Button(window, text="Rzuć kostką", width=8, height=2, font=10, bg="paleturquoise", bd=2, command=roll) 
+roll_button.pack(padx=10, pady=15)   
+
+label = Label(window, font=("times", 40), bg="darkgrey", fg="black") 
+  
+window.mainloop() 
