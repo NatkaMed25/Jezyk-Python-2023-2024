c26c8337e656796f518a3f0c1b53e32cd433fc92
Natalia Mędrek <147911714+NatkaMed25@users.noreply.github.com>
2024-01-10 09:11:54 +0100

    Rozwiązanie zestawu nr 10

--- Zestaw nr 10/zadanie_10.1.py
+++ Zestaw nr 10/zadanie_10.1.py
@@ -9,8 +9,7 @@ window.resizable(0, 0)
   
 def roll(): 
     dice_dots = ['1', '2', '3', '4', '5', '6'] 
-    label.configure( 
-        text=f'{random.choice(dice_dots)}') 
+    label.configure(text=f'{random.choice(dice_dots)}') 
     label.pack() 
    
 roll_button = Button(window, text="Rzuć kostką", width=8, height=2, font=10, bg="paleturquoise", bd=2, command=roll) 
